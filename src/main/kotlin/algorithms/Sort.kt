f5783d6552e2bb23ce5112a4cf85a5776b0c75c4
Vitor <vitor-27@hotmail.com>
2021-09-15 00:41:15 -0300

    Add Mergesort algorithm

--- src/main/kotlin/algorithms/Sort.kt
+++ src/main/kotlin/algorithms/Sort.kt
@@ -54,11 +54,56 @@ class Sort {
             maxHeapify(heap, largest, n)
         }
     }
+
+    private fun merge(list1: List<Int>, list2: List<Int>): List<Int> {
+        var i = 0
+        var j = 0
+        val mergedList = mutableListOf<Int>()
+
+        while(i < list1.size && j < list2.size) {
+            if (list1[i] < list2[j]) {
+                mergedList.add(list1[i])
+                i++
+            } else {
+                mergedList.add(list2[j])
+                j++
+            }
+        }
+
+        if (i < list1.size) mergedList.addAll(list1.subList(i, list1.size))
+        if (j < list2.size) mergedList.addAll(list2.subList(j, list2.size))
+
+        return mergedList
+    }
+
+    private fun mergeSort(list: List<Int>, start: Int, end: Int): List<Int> {
+        if (start == end) {
+            return listOf(list[start])
+        }
+
+        val middleIndex = (start + end) / 2
+
+        val list1 = mergeSort(list, start, middleIndex)
+        val list2 = mergeSort(list, middleIndex + 1, end)
+
+        return merge(list1, list2)
+    }
+
+    fun mergeSort(list: List<Int>): List<Int> {
+        if (list.isEmpty()) return list
+        return mergeSort(list, 0, list.size - 1)
+    }
 }
 
 fun main() {
-    val heap = mutableListOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    val result = Sort().mergeSort(SortTests.test3())
+    print(result)
+}
 
-    Sort().heapSortInPlace(heap)
-    print(heap)
+object SortTests {
+    fun test1() = mutableListOf(3, 2, 1)
+    fun test2() = mutableListOf(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    fun test3() = mutableListOf(2, 4, 1, 10, 1, 2, 5, 7, 8, 1, 9)
+    fun test4() = mutableListOf(1)
+    fun test5() = mutableListOf<Int>()
 }
